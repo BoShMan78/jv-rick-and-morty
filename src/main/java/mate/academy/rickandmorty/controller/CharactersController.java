c303040c0899553c40f0a5cee825f36576eb3ea2
Bohdan Shevtsov <shevtsov.b@gmail.com>
2024-07-02 18:21:56 +0300

    Swagger was added

--- src/main/java/mate/academy/rickandmorty/controller/CharactersController.java
+++ src/main/java/mate/academy/rickandmorty/controller/CharactersController.java
@@ -1,5 +1,7 @@
 package mate.academy.rickandmorty.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import mate.academy.rickandmorty.dto.external.CharacterDtoExt;
@@ -15,18 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/characters")
+@Tag(name = "Rick and Morty character management",
+        description = "Endpoints for managing characters")
 public class CharactersController {
     private final CharactersClient charactersClient;
     private final CharacterService characterService;
     private final CharacterRepository characterRepository;
 
     @GetMapping("/random")
+    @Operation(summary = "Get random character",
+            description = "Fill DB if empty, get random character")
     public Character getRandomCharacter() {
         fillRepository();
         return characterService.getRandomCharacter();
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Search by name",
+            description = "Fill DB if empty, search character by name")
     public List<Character> searchCharacters(@RequestParam String name) {
         fillRepository();
         return characterService.findCharactersByName(name);
